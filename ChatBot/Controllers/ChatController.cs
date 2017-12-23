3f4b04e9165a5014415608a7ca5e55d373256334
Yan Zhang <zhangyan612@gmail.com>
2017-12-23 12:47:04 -0600

    add all chat services

---
+++ ChatBot/Controllers/ChatController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using ChatBot.Models;
+
+namespace ChatBot.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Chat")]
+    public class ChatController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public ChatController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        /// <summary>
+        /// Get bot message reply
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult Get(string msg, string user)
+        {
+            string RootPath = _hostingEnvironment.ContentRootPath;
+            string languageFile = Path.Combine(RootPath, @"App_Data\english.simlpk");
+
+            var response = ChatService.GetResponse(msg, user, languageFile);
+            return Json(response);
+        }
+    }
+}
---
+++ ChatBot/Models/ChatService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Syn.Bot.Siml;
+using System.Web;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ChatBot.Models
+{
+    public class ChatService
+    {
+        public static string GetResponse(string msg, string user, string languageFile)
+        {
+            var simlBot = new SimlBot();
+            simlBot.PackageManager.LoadFromString(File.ReadAllText(languageFile));
+            var result = simlBot.Chat(msg);
+
+            // save it to database
+            //db.InsertChat(msg, result.BotMessage, user);
+            return result.BotMessage;
+        }
+
+    }
+}
